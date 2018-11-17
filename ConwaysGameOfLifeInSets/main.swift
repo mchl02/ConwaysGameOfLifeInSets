230f52a53dc8a6fd2a3fc6ed4d5a479845f2b5b4
Matt <matthewchlee02@gmail.com>
2018-11-16 22:22:08 -0500

    Initial Commit

---
+++ ConwaysGameOfLifeInSets/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  ConwaysGameOfLifeInSets
+//
+//  Created by ws on 11/16/18.
+//  Copyright © 2018 mchl02. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
